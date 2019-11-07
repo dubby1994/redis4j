7251b6653ce49ab5095661b3a957edbedc53819f
daoxuan <daoxuan@pinduoduo.com>
2019-11-07 17:56:29 +0800

    ut

--- src/test/java/cn/dubby/redis4j/test/RedisTemplateTest.java
+++ src/test/java/cn/dubby/redis4j/test/RedisTemplateTest.java
@@ -41,8 +41,6 @@ public class RedisTemplateTest {
         countDownLatch.await();
         System.out.println("==== LOOP COMPLETE ====");
 
-        System.out.println(redisTemplate.info().get());
-
         redisTemplate.close();
     }
 
